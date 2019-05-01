6421b5cfcef3bc5bfb9bb5af2d8e6ff6b20e941d
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2019-04-30 21:33:20 -0600

    setup all routes and nav bar

--- functions/index.js
+++ functions/index.js
@@ -21,6 +21,54 @@ app.get('/about', function(request, response) {
 	});
 });
 
+app.get('/news', function(request, response) {
+	response.render('pages/news', {
+		name: 'news'
+	});
+});
+
+app.get('/workshops', function(request, response) {
+	response.render('pages/workshops', {
+		name: 'workshops'
+	});
+});
+
+app.get('/stempower18', function(request, response) {
+	response.render('pages/stempower18', {
+		name: 'STEMpower'
+	});
+});
+
+app.get('/stempower17', function(request, response) {
+	response.render('pages/stempower17', {
+		name: 'STEMpower'
+	});
+});
+
+app.get('/fuse', function(request, response) {
+	response.render('pages/fuse', {
+		name: 'STEMpower'
+	});
+});
+
+app.get('/ccd', function(request, response) {
+	response.render('pages/ccd', {
+		name: 'YouthCompete'
+	});
+});
+
+app.get('/lwb', function(request, response) {
+	response.render('pages/lwb', {
+		name: 'YouthCompete'
+	});
+});
+
+app.get('/madebyouth', function(request, response) {
+	response.render('pages/madebyouth', {
+		name: 'madebyouth'
+	});
+});
+
 app.get('/contact', function(request, response) {
 	response.render('pages/contact', {
 		name: 'contact'
