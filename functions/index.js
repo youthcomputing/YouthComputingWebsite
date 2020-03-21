0f3a6873d1cc87b04f4e92b7eecc327f30ab8352
KrishKrosh <krish.life21@gmail.com>
2020-03-21 17:44:15 -0600

    added pages to index.js

--- functions/index.js
+++ functions/index.js
@@ -1,66 +1,74 @@
-const functions = require ('firebase-functions');
-const express = require ('express');
+const functions = require("firebase-functions");
+const express = require("express");
 
 // load the things we need
-const app = express ();
+const app = express();
 
 // set the view engine to ejs
-app.set ('views', './views');
-app.set ('view engine', 'ejs');
+app.set("views", "./views");
+app.set("view engine", "ejs");
 
 // index page
-app.get ('/', function (request, response) {
-  response.render ('pages/index', {
-    name: 'index',
+app.get("/", function(request, response) {
+  response.render("pages/index", {
+    name: "index"
   });
 });
 
-app.get ('/about', function (request, response) {
-  response.render ('pages/about', {
-    name: 'about',
+app.get("/about", function(request, response) {
+  response.render("pages/about", {
+    name: "about"
   });
 });
 
-app.get ('/recent', function (request, response) {
-  response.render ('pages/recent', {
-    name: 'news',
+app.get("/recent", function(request, response) {
+  response.render("pages/recent", {
+    name: "news"
   });
 });
 
-app.get ('/workshops', function (request, response) {
-  response.render ('pages/workshops', {
-    name: 'workshops',
+app.get("/workshops", function(request, response) {
+  response.render("pages/workshops", {
+    name: "workshops"
+  });
+});
+app.get("/stempower19", function(request, response) {
+  response.render("pages/stempower19", {
+    name: "STEMpower"
+  });
+});
+app.get("/stempower18", function(request, response) {
+  response.render("pages/stempower18", {
+    name: "STEMpower"
   });
 });
 
-app.get ('/stempower18', function (request, response) {
-  response.render ('pages/stempower18', {
-    name: 'STEMpower',
+app.get("/stempower17", function(request, response) {
+  response.render("pages/stempower17", {
+    name: "STEMpower"
   });
 });
 
-app.get ('/stempower17', function (request, response) {
-  response.render ('pages/stempower17', {
-    name: 'STEMpower',
+app.get("/fuse17", function(request, response) {
+  response.render("pages/fuse17", {
+    name: "fuse"
+  });
+});
+app.get("/fuse19", function(request, response) {
+  response.render("pages/fuse19", {
+    name: "fuse"
+  });
+});
+app.get("/junto", function(request, response) {
+  response.render("pages/junto", {
+    name: "junto"
   });
 });
 
-app.get ('/fuse17', function (request, response) {
-  response.render ('pages/fuse17', {
-    name: 'STEMpower',
+app.get("/contact", function(request, response) {
+  response.render("pages/contact", {
+    name: "contact"
   });
 });
 
-app.get ('/junto', function (request, response) {
-  response.render ('pages/junto', {
-    name: 'junto',
-  });
-});
-
-app.get ('/contact', function (request, response) {
-  response.render ('pages/contact', {
-    name: 'contact',
-  });
-});
-
-exports.app = functions.https.onRequest (app);
+exports.app = functions.https.onRequest(app);
